2d627490aea92a3be7191242cf11abf5ca1f54ee
Nidhal Mouldi <mouldinidhal@gmail.com>
2022-10-08 23:09:16 +0200

    Initial commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $prix_achat;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix_vente;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $reference;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrixAchat(): ?float
+    {
+        return $this->prix_achat;
+    }
+
+    public function setPrixAchat(?float $prix_achat): self
+    {
+        $this->prix_achat = $prix_achat;
+
+        return $this;
+    }
+
+    public function getPrixVente(): ?float
+    {
+        return $this->prix_vente;
+    }
+
+    public function setPrixVente(float $prix_vente): self
+    {
+        $this->prix_vente = $prix_vente;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+}
